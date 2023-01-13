463b16af5536bf8f11cb6eff2cc6e3bd199a246a
lhss <121652275+leesohun@users.noreply.github.com>
2023-01-13 18:06:22 +0900

    Update keras34_2_cifar10.py

--- keras34_2_cifar10.py
+++ keras34_2_cifar10.py
@@ -1,5 +1,6 @@
 import numpy as np
 from tensorflow.keras.datasets import cifar10, cifar100
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 #1. 데이터
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -11,6 +12,45 @@ print(np.unique(y_train, return_counts=True))
 #  (array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=uint8), array([5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000],
 #       dtype=int64))
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, Flatten
+
+#2. 모델
+model = Sequential()
+model.add(Conv2D(filters=128, kernel_size=(2,2), input_shape=(32, 32, 3), activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(2,2)))  
+model.add(Conv2D(filters=64, kernel_size=(2,2)))  
+model.add(Flatten())                             
+model.add(Dense(32, activation='relu'))          
+model.add(Dense(10, activation='softmax'))
+
+
+#3. 컴파일, 훈련
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam',
+              metrics=['acc'])
+model.fit(x_train, y_train, epochs=100, verbose=1, batch_size=128,
+          validation_split=0.2,)
+
+es= EarlyStopping(monitor='val_loss', patience=20, mode = 'min',
+                               restore_best_weights=True,
+                               verbose=1)
+mcp = ModelCheckpoint(monitor='val_loss',  mode='auto', verbose=1,
+                       save_best_only=True)
+
+
+
+
+#4. 평가, 예측
+results = model.evaluate(x_test, y_test)
+print('loss : ', results[0])
+print('acc :', results[1])
+
+
+'''
+
+
+
+'''
 
 
 
