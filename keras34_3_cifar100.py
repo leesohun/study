8b296b69d61b91ac0e42e28d790975dc4f775fa7
lhss <121652275+leesohun@users.noreply.github.com>
2023-01-13 22:11:05 +0900

    Update keras34_3_cifar100.py

--- keras34_3_cifar100.py
+++ keras34_3_cifar100.py
@@ -2,6 +2,13 @@ import numpy as np
 from tensorflow.keras.datasets import cifar10, cifar100
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
+
+path = './_save'
+filepath = './_save/MCP/'
+filename = '{epoch:04d}-{val_loss: 4f}.hdf5' 
+
+
+
 #1. 데이터
 (x_train, y_train), (x_test, y_test) = cifar100.load_data()
 
@@ -16,7 +23,7 @@ from tensorflow.keras.layers import Conv2D, Dense, Flatten
 
 #2. 모델
 model = Sequential()
-model.add(Conv2D(filters=128, kernel_size=(2,2), input_shape=(28, 28, 1), activation='relu'))
+model.add(Conv2D(filters=128, kernel_size=(2,2), input_shape=(32, 32, 3), activation='relu'))
 model.add(Conv2D(filters=64, kernel_size=(2,2)))  
 model.add(Conv2D(filters=64, kernel_size=(2,2)))  
 model.add(Flatten())                             
@@ -33,10 +40,22 @@ model.fit(x_train, y_train, epochs=100, verbose=1, batch_size=256,
 es= EarlyStopping(monitor='val_loss', patience=20, mode = 'min',
                                restore_best_weights=True,
                                verbose=1)
+import datetime 
+date = datetime.datetime.now()                           
+print(date)                         
+print(type(date))                 
+date = date.strftime("%m%d_%H%M")                       
+print(date)                               
+print(type(date))   
+
+
+
+
 mcp = ModelCheckpoint(monitor='val_loss',  mode='auto', verbose=1,
-                       save_best_only=True)
+                       save_best_only=True,
+                        filepath= filepath + 'k34_3_' + date +'_'+filename)
 
-
+model.save(path+'keras34_3_mnist_save_model.h5')
 
 
 #4. 평가, 예측
@@ -46,8 +65,6 @@ print('acc :', results[1])
 
 
 '''
-
-
 
 '''
 
