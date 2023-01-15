8e1d2b2f06a53b99507ea8ee0c0c9bae7f30f43a
lhss <121652275+leesohun@users.noreply.github.com>
2023-01-15 23:32:28 +0900

    Update keras16_validation8_dacon.ddarung.py

--- keras16_validation8_dacon.ddarung.py
+++ keras16_validation8_dacon.ddarung.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import pandas as pd
+
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
