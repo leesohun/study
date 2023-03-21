fc74c9c5e8d56d5ab9c6d8139cf6dcbd53d5ec5d
lhss <121652275+leesohun@users.noreply.github.com>
2023-03-21 23:53:17 +0900

    Update keras05_batch.py

--- keras05_batch.py
+++ keras05_batch.py
@@ -20,6 +20,7 @@ model.compile(loss='mae', optimizer='adam')
 model.fit(x, y, epochs=10, batch_size=4)
 #batch size란 하나의 미니 배치에 넘겨주는 데이터의 개수로 배치 사이즈가
 #7이면 6과 같이 1이 된다 batch size의 디폴트 값은 32이다
+
 #데이터가 ([1,2,3,4,5,6]) 인 경우에 batch size가 1이면 6/6 2이면 3/3 
 #3,4,5의 경우에는 모두 2/2 6의 경우에는 1/1이 된다
 
